e341391941fd4eefa937938cd027e67d3dc6172b
ferhatgec <ferhatgectao@gmail.com>
2020-08-06 02:55:20 +0300

    Added version and removed info Hah. This link redirects to my GitHub profile. :^)

--- FileSystemPlusPlus.h
+++ FileSystemPlusPlus.h
@@ -1,5 +1,5 @@
 /*# MIT License
-# Forked from https://github.com/FerhatGec/fsplusplus
+# 
 # Copyright (c) 2020 Ferhat Geçdoğan All Rights Reserved.
 # Distributed under the terms of the MIT License.
 #*/
@@ -21,6 +21,8 @@
 #include <unistd.h>
 #define GetCurrentDir getcwd
 #endif
+
+#define FS_PLUS_PLUS_VERSION "0.2-beta-1"
 
 namespace fsplusplus {
 	static std::string EraseAllSubString(std::string & mainString, const std::string & erase) {
